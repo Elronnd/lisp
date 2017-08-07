c6b2a2ee5bbe95a2233f8c7dd3bebab380a0d980
Elronnd <elronnd@protonmail.ch>
2017-08-06 20:03:15 -0700

    Add simple (arithmetic) ast, and an evaluator

--- src/main.c
+++ src/main.c
@@ -4,6 +4,130 @@
 #include <stdlib.h>
 #include <editline/readline.h>
 
+/*
+ * (+ (* 5 7) 3)
+ *
+ * isval = false
+ * op = '+'
+ * childs = :
+ *     { isval = true
+ *       val = 3
+ *     }
+ *     { isval = false
+ *       op = '*'
+ *       childs = :
+ *       { isval = true
+ *         val = 5
+ *       }
+ *       { isval = true
+ *         val = 7
+ *       }
+ *     }
+ *
+ *     To evaluate:
+ *	 if isexpr:
+ *	   map/reduce eval() of op on each child
+ *	 else:
+ *	   return val
+ */
+
+
+typedef struct Ast {
+	bool isval;
+
+	union {
+		// c11 ftw!
+		struct {
+			struct Ast *childs;
+			size_t numchilds;
+		};
+
+		long val;
+	};
+
+
+	char op;
+} Ast;
+
+long parseast(Ast ast) {
+	if (ast.isval) {
+		return ast.val;
+	} else {
+		long *vals = malloc(sizeof(long) * ast.numchilds);
+		long tmp;
+
+		for (size_t i = 0; i < ast.numchilds; i++) {
+			vals[i] = parseast(ast.childs[i]);
+		}
+
+		switch (ast.op) {
+			case '+': tmp = vals[0] + vals[1]; break;
+			case '-': tmp = vals[0] - vals[1]; break;
+			case '*': tmp = vals[0] * vals[1]; break;
+			case '/': tmp = vals[0] / vals[1]; break;
+			default: printf("Unknown operation %c!!", ast.op); tmp = -1;
+		}
+
+		return tmp;
+	}
+}
+/* (+ (* 5 7) 3)
+ *
+ * isval = false
+ * op = '+'
+ * childs = :
+ *     { isval = true
+ *       val = 3
+ *     }
+ *     { isval = false
+ *       op = '*'
+ *       childs = :
+ *       { isval = true
+ *         val = 5
+ *       }      
+ *       { isval = true
+ *         val = 7
+ *       }      
+ *     }
+ *
+ *     To evaluate:     
+ *       if isexpr:     
+ *         map/reduce eval() of op on each child
+ *       else:  
+ *         return val   
+ */                     
+
+int main(void) {
+	Ast ast;
+
+	ast.isval = false;
+	ast.op = '+';
+	ast.childs = malloc(sizeof(ast) * 2);
+	ast.numchilds = 2;
+
+	ast.childs[0].isval = true;
+	ast.childs[0].val = 3;
+
+	ast.childs[1].isval = false;
+	ast.childs[1].op = '*';
+	ast.childs[1].childs = malloc(sizeof(ast) * 2);
+	ast.childs[1].numchilds = 2;
+
+	ast.childs[1].childs[0].isval = true;
+	ast.childs[1].childs[0].val = 5;
+	ast.childs[1].childs[1].isval = true;
+	ast.childs[1].childs[1].val = 7;
+
+	printf("Values is %ld", parseast(ast));
+}
+
+#if 0
+
+
+
+
+
+
 
 int main(void) {
 	bool done = false;
@@ -29,3 +153,4 @@ int main(void) {
 
 	return 0;
 }
+#endif
