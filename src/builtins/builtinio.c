f2d0b116a4f3d7fb0eab8e34d31a29a60ad41219
Elronnd <elronnd@protonmail.ch>
2017-08-08 19:20:51 -0700

    Actually add builtinio.c.  Everything should work now

---
+++ src/builtins/builtinio.c
@@ -0,0 +1,17 @@
+#include "build-lisp.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+Lval builtin_readchar(Lval *vals, lint numvals) {
+	// to appease the compiler
+	(void) vals;
+	(void) numvals;
+
+	Lval tmp = {.type = LTYPE_STR, .str = malloc(2)};
+
+	tmp.str[1] = '\0';
+	tmp.str[0] = getchar();
+
+	return tmp;
+}
